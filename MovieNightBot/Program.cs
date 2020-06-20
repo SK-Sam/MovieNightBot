fbed95484781bf017157017e21bf2a38b66803ef
Sam <SK-Sam@users.noreply.github.com>
2020-06-20 00:29:51 -0400

    initial commit

---
+++ MovieNightBot/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MovieNightBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        private DiscordSocketClient _client;
+        private CommandService _commands;
+        private IServiceProvider _services;
+        public async Task RunBotAsync()
+        {
+            _client = new DiscordSocketClient();
+            _commands = new CommandService();
+            _services = new ServiceCollection()
+                .AddSingleton(_client)
+                .AddSingleton(_commands)
+                .AddSingleton<ConfigHandler>()
+                .BuildServiceProvider();
+            await _services.GetService<ConfigHandler>().PopulateConfig();
+
+            await _client.LoginAsync(TokenType.Bot, _services.GetService<ConfigHandler>().GetToken());
+            await _client.StartAsync();
+        }
+    }
+}

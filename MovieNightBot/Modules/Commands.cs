7a84bd25a357f9449d743376bbd3680de4ecbd48
Sam <SK-Sam@users.noreply.github.com>
2020-06-21 08:27:14 -0400

    Added commands. Omitted list command due to requirement to add database.

--- MovieNightBot/Modules/Commands.cs
+++ MovieNightBot/Modules/Commands.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,25 +10,48 @@ namespace MovieNightBot.Modules
 {
     public class Commands : ModuleBase<SocketCommandContext>
     {
-        [Command("bing")]
+        
+        List<string> movie_list = new List<string>();
+
+        [Command("help")]
         public async Task Ping()
         {
-            await ReplyAsync("Bong");
+            await ReplyAsync("Commands: \n!ayy: I will Lmao.\n" +
+                                            "!add: I will add movie title to list of movies.\n" +
+                                            "!adrian: I will append the proper emoji which describes said individual.\n" +
+                                            "!list: I will show every movie on the list.").ConfigureAwait(false);
         }
+
         [Command("ayy")]
         public async Task Lmao()
         {
-            await ReplyAsync("lmao");
+            await ReplyAsync("lmao").ConfigureAwait(false); ;
         }
-        [Command("hell")]
-        public async Task Yeah()
+        [Command("add")]
+        public async Task Add([Remainder]string movie)
         {
-            await ReplyAsync("yeah");
+            
+            await ReplyAsync("Adding \"" + movie + "\" to the list.").ConfigureAwait(false); ;
+            movie_list.Add(movie);
+            
         }
+
         [Command("adrian")]
         public async Task Rat()
         {
-            await ReplyAsync(":rat:");
+            await ReplyAsync(":rat:").ConfigureAwait(false); ;
         }
+
+        /*[Command("list")]
+        public async Task List()
+        {
+            string view_movie_list = "";
+            for(int i = 0; i < movie_list.Count; i++)
+            {
+                int counter = i + 1;
+                view_movie_list = view_movie_list + counter.ToString() + ": \"" + movie_list[i] + "\"\n";
+            }
+            await ReplyAsync(view_movie_list).ConfigureAwait(false); ;
+        }*/
     }
 }

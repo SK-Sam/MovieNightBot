dc146ee75c2ea502b8bc0d33d7875737c120e9ab
Sam <SK-Sam@users.noreply.github.com>
2020-06-22 04:16:16 -0400

    Movies are added and listed as Title Case. Added roll function to decide on a random movie to watch inside list. Updated help menu.

--- MovieNightBot/Modules/Commands.cs
+++ MovieNightBot/Modules/Commands.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,8 @@ namespace MovieNightBot.Modules
 {
     public class Commands : ModuleBase<SocketCommandContext>
     {
+        Random random = new Random();
+        TextInfo myTI = new CultureInfo("en-US", false).TextInfo;
         
         static List<string> movie_list = new List<string>();
 
@@ -18,29 +21,21 @@ namespace MovieNightBot.Modules
         {
             await ReplyAsync("Commands: \n!ayy: I will Lmao.\n" +
                                             "!add: I will add movie title to list of movies.\n" +
-                                            "!adrian: I will append the proper emoji which describes said individual.\n" +
-                                            "!list: I will show every movie on the list.").ConfigureAwait(false);
+                                            "!list: I will show every movie on the list.\n" +
+                                            "!remove: I will remove said movie from the movie list.\n" +
+                                            "!roll: I will help your indecisiveness by rolling dice to figure out which movie to watch.\n" +
+                                            "!(name): I will append the proper emoji which describes said individual.\n", true).ConfigureAwait(false);
         }
 
-        [Command("ayy")]
-        public async Task Lmao()
-        {
-            await ReplyAsync("lmao", true).ConfigureAwait(false); ;
-        }
         [Command("add")]
         public async Task Add([Remainder] string movie)
         {
 
-            await ReplyAsync("Adding \"" + movie + "\" to the list.", true).ConfigureAwait(false); ;
-            movie_list.Add(movie);
+            await ReplyAsync("Adding \"" + myTI.ToTitleCase(movie) + "\" to the list.", true).ConfigureAwait(false); ;
+            movie_list.Add(myTI.ToTitleCase(movie));
 
         }
 
-        [Command("adrian")]
-        public async Task Rat()
-        {
-            await ReplyAsync(":rat:").ConfigureAwait(false); ;
-        }
 
         [Command("list")]
         public async Task List()
@@ -51,7 +46,42 @@ namespace MovieNightBot.Modules
                 int counter = i + 1;
                 view_movie_list = view_movie_list + counter.ToString() + ": \"" + movie_list[i] + "\"\n";
             }
-            await ReplyAsync(view_movie_list).ConfigureAwait(false); ;
+            await ReplyAsync(view_movie_list, true).ConfigureAwait(false); ;
+        }
+
+        [Command("remove")]
+        public async Task Remove([Remainder] string movie)
+        {
+            if (movie_list.Contains(myTI.ToTitleCase(movie)))
+            {
+                movie_list.Remove(myTI.ToTitleCase(movie));
+                await ReplyAsync(movie + " removed from list.", true).ConfigureAwait(false);
+            }
+            else
+            {
+                await ReplyAsync(myTI.ToTitleCase(movie) + " is not in the list.", true).ConfigureAwait(false);
+            }
+        }
+
+        [Command("roll")]
+        public async Task Roll()
+        {
+            int list_max = movie_list.Count;
+            int random_number = random.Next(list_max) + 1;
+
+            await ReplyAsync("You rolled " + random_number + ". We're watching... " + myTI.ToTitleCase(movie_list[random_number]) + "!").ConfigureAwait(false);
+        }
+        //Fun commands
+        [Command("ayy")]
+        public async Task Lmao()
+        {
+            await ReplyAsync("lmao", true).ConfigureAwait(false); ;
+        }
+
+        [Command("adrian")]
+        public async Task Rat()
+        {
+            await ReplyAsync(":rat:").ConfigureAwait(false);
         }
     }
 }
